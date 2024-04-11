eb2be589be364cb82c2f58379a6072793c27ded9
Kim Andersson <kandersson@gmail.com>
2024-04-11 12:15:07 +0200

    routes.js deleted - old config - not needed

--- src/router/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const broadcastRoutes = require("./broadcastRoutes");
-const channelRoutes = require("./channelRoutes");
-
-router.use("/api", broadcastRoutes);
-router.use("/api", channelRoutes);
-
-export default router;
